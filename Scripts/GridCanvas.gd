e5e9c1ce953dee4e7aa1e91a4754ed0125ad6721
Boris Dimitrov <boris1234dimitrov@gmail.com>
2025-01-10 18:00:05 +0200

    Added point movement and count modification

--- Scripts/GridCanvas.gd
+++ Scripts/GridCanvas.gd
@@ -11,30 +11,36 @@ extends Node2D
 var points : Array[Marker2D]
 var selected_point : Marker2D = null
 
-func _ready():
+func _ready() -> void:
+	%AddPointButton.pressed.connect(_on_add_point_button_pressed)
+	%RemovePointButton.pressed.connect(_on_remove_point_button_pressed)
+	
 	for child in get_children():
 		var point := child as Marker2D
 		if point:
 			points.append(point)
 
-func _input(event):
+func _input(event) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if selected_point and not event.pressed:
 			# Stop dragging if the button is released.
 			selected_point = null
 		else:
 			for point in points:
-				if (event.position - point.position).length() < select_radius:
+				if (event.position - point.global_position).length() < select_radius:
 					# Start dragging if the click is on the sprite.
 					if not selected_point and event.pressed:
 						selected_point = point
 
 	if event is InputEventMouseMotion and selected_point:
 		# While dragging, move the sprite with the mouse.
-		selected_point.position = event.position
-		
+		selected_point.global_position = event.position
+		queue_redraw()
 
-func _draw():
+func _physics_process(delta: float) -> void:
+	points.sort_custom(func(p1,p2): return p1.global_position.x < p2.global_position.x)
+
+func _draw() -> void:
 	var viewport_size = get_viewport_rect().size
 
 	# Draw vertical grid lines
@@ -45,9 +51,26 @@ func _draw():
 	for y in range(0, int(viewport_size.y), grid_size):
 		draw_line(Vector2(0, y), Vector2(viewport_size.x, y), grid_color, 1)
 	
+	# Draw the points and the lines between the them
 	for i in range(points.size()):
-		if i == 0: continue
-		draw_line(points[i - 1].position, points[i].position, line_color, 2)
+		var p_pos = points[i].global_position
+		draw_circle(p_pos, 3, Color.RED)
+		draw_string(ThemeDB.fallback_font, p_pos + Vector2(-10, -30), \
+		 	"(%d, %d)" % [p_pos.x, p_pos.x], HORIZONTAL_ALIGNMENT_CENTER)
+		if i > 0:
+			draw_line(points[i - 1].global_position, p_pos, line_color, 2)
+
+func _on_add_point_button_pressed():
+	var new_point = Marker2D.new()
+	add_child(new_point)
 	
-	for point in points:
-		draw_circle(point.position, 3, Color.RED)
+	var viewport_size = get_viewport_rect().size
+	new_point.global_position.x = randf_range(0, viewport_size.x)
+	new_point.global_position.y = randf_range(0, viewport_size.y)
+	points.push_back(new_point)
+	queue_redraw()
+	
+func _on_remove_point_button_pressed():
+	var removed_point : Marker2D = points.pop_back()
+	removed_point.queue_free()
+	queue_redraw()
