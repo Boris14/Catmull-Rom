ec56da11dae79e5fecf36d8f4c42d52eae18342b
Boris Dimitrov <boris1234dimitrov@gmail.com>
2025-01-14 00:12:02 +0200

    Created a curve

--- Scripts/GridCanvas.gd
+++ Scripts/GridCanvas.gd
@@ -7,6 +7,7 @@ extends Node2D
 @export var line_color := Color.RED
 @export var points_color := Color.RED
 @export var select_radius := 20.0
+@export var curve_segments := 100
 
 var points : Array[Marker2D]
 var selected_point : Marker2D = null
@@ -38,7 +39,29 @@ func _input(event) -> void:
 		queue_redraw()
 
 func _physics_process(delta: float) -> void:
-	points.sort_custom(func(p1,p2): return p1.global_position.x < p2.global_position.x)
+	pass
+	#points.sort_custom(func(p1,p2): return p1.global_position.x < p2.global_position.x)
+
+func get_curve_point(points, t):
+	var constructed_points = []
+	for point in points:
+		constructed_points.push_back(point.global_position)
+	while constructed_points.size() > 1:		
+		var new_points = []
+		for i in range(1, constructed_points.size()):
+			var next_point_pos = (1 - t) * constructed_points[i - 1] + t * constructed_points[i]
+			new_points.push_back(next_point_pos)
+		constructed_points = new_points
+	return constructed_points[0]
+
+func get_curve_points(points) -> PackedVector2Array:
+	var result : PackedVector2Array
+	var t := 0.0
+	var step := 1.0 / curve_segments
+	while t < 1.0:
+		result.append(get_curve_point(points, t))
+		t += step
+	return result
 
 func _draw() -> void:
 	var viewport_size = get_viewport_rect().size
@@ -51,14 +74,16 @@ func _draw() -> void:
 	for y in range(0, int(viewport_size.y), grid_size):
 		draw_line(Vector2(0, y), Vector2(viewport_size.x, y), grid_color, 1)
 	
-	# Draw the points and the lines between the them
+	# Draw the points and the lines between them
 	for i in range(points.size()):
 		var p_pos = points[i].global_position
-		draw_circle(p_pos, 3, Color.RED)
+		draw_circle(p_pos, 4, Color.RED)
 		draw_string(ThemeDB.fallback_font, p_pos + Vector2(-10, -30), \
 		 	"(%d, %d)" % [p_pos.x, p_pos.x], HORIZONTAL_ALIGNMENT_CENTER)
 		if i > 0:
 			draw_line(points[i - 1].global_position, p_pos, line_color, 2)
+			
+	draw_polyline(get_curve_points(points), Color.GREEN_YELLOW)
 
 func _on_add_point_button_pressed():
 	var new_point = Marker2D.new()
